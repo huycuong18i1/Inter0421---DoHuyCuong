6b532d3c2b1a5817289b11a296df8cb127bdd896
DoHuyCuong <dhcuong.18i@cit.udn.vn>
2021-04-16 10:33:10 +0700

    thêm comment sửa task5

--- _01_database/basic.sql
+++ _01_database/basic.sql
@@ -1,18 +1,41 @@
 use basic;
 -- task2 
+-- Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là
+-- một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 ký tự.
 SELECT * FROM nhanvien where HoTen like 'H%' or HoTen like 'T%' or HoTen like 'K%' and length(HoTen) < 15;
+
 -- task3
+-- Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và
+-- có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
 SELECT * FROM khachhang where ((2021 - year(NgaySinh)) > 18 and (2021 - year(NgaySinh)) < 50) and DiaChi in ('Đà Nẵng', 'Quảng Trị');
--- task4 
+
+-- task4
+-- Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu
+-- lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của
+-- khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là
+-- “Diamond”.
 select khachhang.IDKhachHang, khachhang.HoTen, count(hopdong.IDHopDong) as solandat from khachhang inner join hopdong on khachhang.IDKhachHang = hopdong.IDKhachHang
 inner join loaikhach on khachhang.IDLoaiKhach = loaikhach.IDLoaiKhach
 where loaikhach.TenLoaiKhach = "Diamond"
 group by khachhang.HoTen order by solandat;
+
 -- task5
+-- Hiển thị IDKhachHang, HoTen, TenLoaiKhach, IDHopDong,
+-- TenDichVu, NgayLamHopDong, NgayKetThuc, TongTien (Với
+-- TongTien được tính theo công thức như sau: ChiPhiThue +
+-- SoLuong*Gia, với SoLuong và Giá là từ bảng DichVuDiKem) cho tất cả
+-- các Khách hàng đã từng đặt phỏng. (Những Khách hàng nào chưa từng
+-- đặt phòng cũng phải hiển thị ra).
 SELECT khachhang.IDKhachHang, khachhang.HoTen, loaikhach.TenLoaiKhach, hopdong.IDHopDong, hopdong.NgayLamHopDong, hopdong.NgayKetThuc, (dichvu.ChiPhiThue + (hopdongchitiet.SoLuong * dichvudikem.Gia)) as TongTien from khachhang 
-inner join loaikhach on khachhang.IDLoaiKhach = loaikhach.IDLoaiKhach
-inner join hopdong on khachhang.IDKhachHang = hopdong.IDKhachHang
-inner join dichvu on hopdong.IDDichVu = dichvu.IDDichVu
-inner join hopdongchitiet on hopdong.IDHopDong = hopdongchitiet.IDHopDong
-inner join dichvudikem on hopdongchitiet.IDDichVuDiKem = dichvudikem.IDDichVuDiKem
+left join loaikhach on khachhang.IDLoaiKhach = loaikhach.IDLoaiKhach
+left join hopdong on khachhang.IDKhachHang = hopdong.IDKhachHang
+left join dichvu on hopdong.IDDichVu = dichvu.IDDichVu
+left join hopdongchitiet on hopdong.IDHopDong = hopdongchitiet.IDHopDong
+left join dichvudikem on hopdongchitiet.IDDichVuDiKem = dichvudikem.IDDichVuDiKem;
 ;
+-- task6
+-- Hiển thị IDDichVu, TenDichVu, DienTich, ChiPhiThue,
+-- TenLoaiDichVu của tất cả các loại Dịch vụ chưa từng được Khách hàng
+-- thực hiện đặt từ quý 1 của năm 2019 (Quý 1 là tháng 1, 2, 3).
+
+ 
