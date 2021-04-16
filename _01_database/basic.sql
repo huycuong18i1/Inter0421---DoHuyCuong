aee8eb0d59026e50a5a9e5f8ef595ecab36e5336
DoHuyCuong <dhcuong.18i@cit.udn.vn>
2021-04-16 17:19:28 +0700

    push task 9

--- _01_database/basic.sql
+++ _01_database/basic.sql
@@ -66,7 +66,16 @@ group by kh.HoTen;
 select khachhang.HoTen from khachhang
 union 
 select khachhang.HoTen from khachhang
-
+;
+-- task9 
+-- Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi
+-- tháng trong năm 2019 thì sẽ có bao nhiêu khách hàng thực hiện đặt
+-- phòng.
+SELECT *, count(*) as sokhachdattrongthang FROM hopdong
+where (month(hopdong.NgayLamHopDong) in (1,2,3,4,5,6,7,8,9,10,11,12) and year(hopdong.NgayLamHopDong) = 2019)
+group by month(hopdong.NgayLamHopDong)
+having count(hopdong.NgayLamHopDong)
+;
  
 
 
