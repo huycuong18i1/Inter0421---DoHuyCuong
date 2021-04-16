bcdab246934b72ab03d190bb33a7ba0f643b7683
DoHuyCuong <dhcuong.18i@cit.udn.vn>
2021-04-16 09:46:54 +0700

    bổ sung task5

--- _01_database/basic.sql
+++ _01_database/basic.sql
@@ -9,4 +9,10 @@ inner join loaikhach on khachhang.IDLoaiKhach = loaikhach.IDLoaiKhach
 where loaikhach.TenLoaiKhach = "Diamond"
 group by khachhang.HoTen order by solandat;
 -- task5
- select * from khachhang full join hopdong, loaikhach, loaidichvu;
+SELECT khachhang.IDKhachHang, khachhang.HoTen, loaikhach.TenLoaiKhach, hopdong.IDHopDong, hopdong.NgayLamHopDong, hopdong.NgayKetThuc, (dichvu.ChiPhiThue + (hopdongchitiet.SoLuong * dichvudikem.Gia)) as TongTien from khachhang 
+inner join loaikhach on khachhang.IDLoaiKhach = loaikhach.IDLoaiKhach
+inner join hopdong on khachhang.IDKhachHang = hopdong.IDKhachHang
+inner join dichvu on hopdong.IDDichVu = dichvu.IDDichVu
+inner join hopdongchitiet on hopdong.IDHopDong = hopdongchitiet.IDHopDong
+inner join dichvudikem on hopdongchitiet.IDDichVuDiKem = dichvudikem.IDDichVuDiKem
+;
